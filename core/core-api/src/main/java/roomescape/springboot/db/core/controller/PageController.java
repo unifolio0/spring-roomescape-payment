8780fd0520300abb6c192afc442eb461c93dd056
unifolio0 <boho82ha@gmail.com>
2025-01-02 16:29:04 +0900

    chore: 패키지명 수정

--- core/core-api/src/main/java/roomescape/springboot/db/core/controller/PageController.java
+++ core/core-api/src/main/java/roomescape/springboot/db/core/controller/PageController.java
@@ -1,4 +1,4 @@
-package roomescape.controller;
+package roomescape.springboot.db.core.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
